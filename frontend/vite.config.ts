b873ef96ba827f3e8742b56a27d29224bf06364e
Aritra <112630505+Aritra-2000@users.noreply.github.com>
2024-12-09 18:52:14 +0530

    some change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  server: {
+    proxy: {
+      "/proxy": {
+        target: "https://medium-blog-buoqu2pk9-aritra2000s-projects.vercel.app/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy/, '')
+      },
     },
-  },
+  }
 })
